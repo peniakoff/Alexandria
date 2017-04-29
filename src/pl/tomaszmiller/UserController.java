bac4dcb5acfc3d4cd5d7feb7f51771d4270574bf
Tomasz Miller <info@tomaszmiller.pl>
2017-04-29 14:04:11 +0200

    rename class file

---
+++ src/pl/tomaszmiller/UserController.java
@@ -0,0 +1,19 @@
+package pl.tomaszmiller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Peniakoff on 29.04.2017.
+ */
+public class ControllerUser implements Initializable {
+
+    
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
